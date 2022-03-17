e8a8626e4bca07642a1dcd42feb0c11e2bbe04f7
mdorr <marciorocha@pelotas.ifsul.edu.br>
2022-03-16 21:46:03 -0300

    Atualizando o settings

--- aniversario_campus_pelotas/settings.py
+++ aniversario_campus_pelotas/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'inscricoes',
     'galerias',
@@ -121,11 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -140,4 +138,5 @@ MEDIA_URL = '/media/'
 # EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 # EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 
-django_heroku.settings(locals())
+if not DEBUG:
+    django_heroku.settings(locals())
